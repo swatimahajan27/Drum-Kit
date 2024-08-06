4b98b6f04e4dd54d69f8af3b177e1cfa114a27e3
swatim <svatimahajan@gmail.com>
2024-08-06 19:51:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+let numberOfDrums = document.querySelectorAll(".drum").length
+for (let i = 0; i < numberOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        let buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+
+    });
+}
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+
+
+});
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+
+        default:
+            break;
+    }
+}
+function buttonAnimation(currentkey){
+let activeButton= document.querySelector("." + currentkey);
+activeButton.classList.add("pressed");
+setTimeout(function(){
+    activeButton.classList.remove("pressed");
+},100)
+
+}
+
+
+//let audio= document.getElementById('tom-1');
+//     function audio1(){new audio('tom-1.mp3')};
+//     audio.play();
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
+
+
+//     alert("I got clicked")
